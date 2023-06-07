767d8d82d663ee2d848fd491f4e3d350f51a2200
Leslie <Kooliesk@gmail.com>
2023-06-08 00:13:57 +0200

    Change photo data type to string

--- db/schema.rb
+++ db/schema.rb
@@ -9,29 +9,32 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-# rubocop:disable Metrics/BlockLength
-ActiveRecord::Schema[7.0].define(version: 20_230_607_131_259) do
-  # These are extensions that must be enabled in order to support this database
+
+ActiveRecord::Schema.define(version: 20_230_607_220_734) do
   enable_extension 'plpgsql'
 
   create_table 'comments', force: :cascade do |t|
     t.string 'text'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.bigint 'user_id', null: false
+    t.bigint 'author_id', null: false
     t.bigint 'post_id', null: false
+    t.index ['author_id'], name: 'index_comments_on_author_id'
     t.index ['post_id'], name: 'index_comments_on_post_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
   create_table 'likes', force: :cascade do |t|
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.bigint 'users_id', null: false
+    t.bigint 'author_id', null: false
     t.bigint 'post_id', null: false
+    t.index ['author_id'], name: 'index_likes_on_author_id'
     t.index ['post_id'], name: 'index_likes_on_post_id'
-    t.index ['users_id'], name: 'index_likes_on_users_id'
   end
+end
+
+ActiveRecord::Schema.define(version: 20_230_607_220_734) do
+  enable_extension 'plpgsql'
 
   create_table 'posts', force: :cascade do |t|
     t.string 'title'
@@ -40,23 +43,26 @@ ActiveRecord::Schema[7.0].define(version: 20_230_607_131_259) do
     t.integer 'likes_counter'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.bigint 'users_id', null: false
-    t.index ['users_id'], name: 'index_posts_on_users_id'
+    t.bigint 'author_id', null: false
+    t.index ['author_id'], name: 'index_posts_on_author_id'
   end
 
   create_table 'users', force: :cascade do |t|
     t.string 'name'
-    t.binary 'photo'
+    t.string 'photo'
     t.string 'bio'
     t.integer 'posts_counter'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
+end
+
+ActiveRecord::Schema.define(version: 20_230_607_220_734) do
+  enable_extension 'plpgsql'
 
   add_foreign_key 'comments', 'posts'
-  add_foreign_key 'comments', 'users'
+  add_foreign_key 'comments', 'users', column: 'author_id'
   add_foreign_key 'likes', 'posts'
-  add_foreign_key 'likes', 'users', column: 'users_id'
-  add_foreign_key 'posts', 'users', column: 'users_id'
+  add_foreign_key 'likes', 'users', column: 'author_id'
+  add_foreign_key 'posts', 'users', column: 'author_id'
 end
-# rubocop:enable Metrics/BlockLength
