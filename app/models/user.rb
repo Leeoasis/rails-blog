2f068954bcd6ce366fd3d26d3cf95b678e06bed3
Leslie <Kooliesk@gmail.com>
2023-06-08 14:10:39 +0200

    Edit users_counter method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :posts, foreign_key: :author_id, dependent: :destroy
 
-  scope :user_recent_posts, -> { find(2).posts.order(created_at: :desc).first(3) }
+  scope :user_recent_posts, ->(user_id) { find(user_id).posts.order(created_at: :desc).limit(3) }
 end
