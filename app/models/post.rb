8a6a5590bd5cf8b712dedcea0c9309f9680d1552
Leslie <Kooliesk@gmail.com>
2023-06-08 14:10:25 +0200

    Edit posts coutner method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  scope :update_posts_counter, -> { User.find(3).update(posts_counter: User.find(3).posts.count) }
-  scope :recent_comments, -> { Comment.where(post_id: 2).order(created_at: :desc).first(5) }
+  def self.update_posts_counter(user_id)
+    user = User.find(user_id)
+    user.update(posts_counter: user.posts.count)
+  end
+
+  scope :recent_comments, ->(post_id) { Comment.where(post_id: post_id).order(created_at: :desc).limit(5) }
 end
