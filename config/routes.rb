a1c08fb197ff481d93a2d69bf58468c3d10a6d8c
Leslie <Kooliesk@gmail.com>
2023-06-13 14:51:50 +0200

    Fix rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
+  resources :users, only: %i[index show] do
     resources :posts, only: [:index]
   end
-  
+
   get '/users/:user_id/posts/:id', to: 'posts#show'
 end
