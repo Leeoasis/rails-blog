8a29639bbf6ed417c76b5deafc1bb4ec204e9238
Leslie <Kooliesk@gmail.com>
2023-06-13 13:08:41 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   get 'posts/index'
   get 'posts/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'users/index'
+  get 'users/show'
+  
 end
