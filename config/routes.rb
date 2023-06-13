68c52415cd00b8975df81fa6f3872ce84f13df5c
Leslie <Kooliesk@gmail.com>
2023-06-13 15:18:43 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: [:index]
+    resources :posts, only: %i[index show]
   end
-
-  get '/users/:user_id/posts/:id', to: 'posts#show'
 end
