9c30ba0ab43b2b9ae4ea36668617a2471c915b3d
Leslie <Kooliesk@gmail.com>
2023-06-15 15:16:09 +0200

    Modify user and comment models

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,7 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def self.update_posts_counter(user_id)
-    user = User.find(user_id)
-    user.update(posts_counter: user.posts.count)
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
-
-  scope :recent_comments, ->(post_id) { Comment.where(post_id:).order(created_at: :desc).limit(5) }
 end
