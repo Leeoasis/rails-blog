4e0ff73c1568896aa982542295dd3d8a0ffe42d0
Leslie <Kooliesk@gmail.com>
2023-06-15 16:12:36 +0200

    Update routes to match urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+  root to: 'users#index'
+  resources :users, only: [:show] do
+    resources :posts, only: %i[index show], param: :author_id
   end
+  resources :posts, only: %i[index show], param: :post_id
 end
