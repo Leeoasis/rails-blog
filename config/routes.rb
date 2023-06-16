eb7f5d5b416670e719b04fa08c97ab6b8d333f9e
Leslie <Kooliesk@gmail.com>
2023-06-16 22:18:08 +0200

    update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
-  resources :users, only: [:show] do
-    resources :posts, only: %i[index show], param: :author_id
+  root 'users#index'
+
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: [:create]
+    end
   end
-  resources :posts, only: %i[index show], param: :post_id
 end
