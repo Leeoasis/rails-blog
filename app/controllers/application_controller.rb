4a580d317612208d18332de809467f8971f1d672
Leslie <Kooliesk@gmail.com>
2023-06-16 22:14:22 +0200

    create the current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        User.first
-    end
+  protect_from_forgery with: :exception
+  before_action :current_user
+
+  def current_user
+    @current_user = User.first
+  end
 end
