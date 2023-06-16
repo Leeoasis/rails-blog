bdf8f102c1ab439de11ed1d1fbd64bf03a058d56
Leslie <Kooliesk@gmail.com>
2023-06-16 22:56:30 +0200

    refactor likes controller as per requirements

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,25 @@
 class LikesController < ApplicationController
-  def new
-    @user = User.find(params[:user_id])
-  end
+  before_action :find_user, only: %i[new create]
+  before_action :find_post, only: :create
+
+  def new; end
 
   def create
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:post_id])
-    @like = @post.likes.new
-    @like.author = @user
+    @like = @post.likes.new(author: @user)
     if @like.save
       redirect_to user_post_path(@user, @post)
     else
       render :new
     end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def find_post
+    @post = Post.find(params[:post_id])
   end
 end
