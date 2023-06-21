77db4435b5343bc498fcf86d5ee81120453a456c
M.Nisar Ali <dr.nisaralig@gmail.com>
2023-06-21 13:38:29 +0500

    Set current user using devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
     @users = User.all
+    @current_user = current_user
   end
-
   def show
     @user = User.find(params[:id])
   end
