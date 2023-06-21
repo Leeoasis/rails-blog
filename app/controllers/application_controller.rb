77d0791f059d681c1eae9e776de708fef870569f
Leslie <Kooliesk@gmail.com>
2023-06-21 10:27:04 +0200

    Set devise routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :current_user
 
-  def current_user
-    @current_user = User.first
+  def set_current_user
+    @current_user = current_user
   end
 end
