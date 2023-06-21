d7213d1e320d9c85ddf6e513bc40f49ea4c01617
Leslie <Kooliesk@gmail.com>
2023-06-22 00:18:59 +0200

    Fix rubocop errors

--- Gemfile
+++ Gemfile
@@ -56,8 +56,8 @@ end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
-  gem 'web-console'
   gem 'letter_opener'
+  gem 'web-console'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
@@ -75,4 +75,4 @@ end
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,14 +95,4 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "saidlaasri3@gmail.com",
-    password: "njxwtrzkumutxouk", 
-    domain: "smtp.gmail.com",
-    openssl_verify_mode: "none",
-  }
 end
