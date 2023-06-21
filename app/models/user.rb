3ff82cc7518cc4fb53ee1692cdc1923cead79074
M.Nisar Ali <dr.nisaralig@gmail.com>
2023-06-21 13:37:18 +0500

    Configure user module to allow authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :likes, foreign_key: :author_id, dependent: :destroy
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :posts, foreign_key: :author_id, dependent: :destroy
-
   def user_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
-
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 end
