4092674c531c61cfca8e70687f2869ef192e5c1a
Leslie <Kooliesk@gmail.com>
2023-06-21 11:06:47 +0200

    fix rubocop errors

--- Gemfile
+++ Gemfile
@@ -48,12 +48,10 @@ gem 'bootsnap', require: false
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
-  gem 'capybara'
   gem 'factory_bot_rails'
   gem 'rails-controller-testing'
   gem 'rspec-rails'
   gem 'shoulda-matchers'
-  gem 'webdrivers'
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,65 +10,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_620_214_416) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.string :text
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: true
 
-  create_table 'comments', force: :cascade do |t|
-    t.string 'text'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'author_id', null: false
-    t.bigint 'post_id', null: false
-    t.index ['author_id'], name: 'index_comments_on_user_id'
-    t.index ['post_id'], name: 'index_comments_on_post_id'
+      t.timestamps
+    end
   end
+end
 
-  create_table 'likes', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'author_id', null: false
-    t.bigint 'post_id', null: false
-    t.index ['author_id'], name: 'index_likes_on_author_id'
-    t.index ['post_id'], name: 'index_likes_on_post_id'
+class CreateLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: true
+
+      t.timestamps
+    end
   end
+end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'title'
-    t.string 'text'
-    t.integer 'comment_counter', default: 0
-    t.integer 'likes_counter', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'author_id', null: false
-    t.integer 'comments_counter', default: 0
-    t.index ['author_id'], name: 'index_posts_on_author_id'
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :text
+      t.integer :comment_counter, default: 0
+      t.integer :likes_counter, default: 0
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.integer :comments_counter, default: 0
+
+      t.timestamps
+    end
   end
+end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'photo'
-    t.string 'bio'
-    t.integer 'posts_counter'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :photo
+      t.string :bio
+      t.integer :posts_counter
+      t.string :email, default: '', null: false
+      t.string :encrypted_password, default: '', null: false
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
+      t.string :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string :unconfirmed_email
+
+      t.timestamps
+
+      t.index :reset_password_token, unique: true
+    end
   end
-
-  add_foreign_key 'comments', 'posts'
-  add_foreign_key 'comments', 'users', column: 'author_id'
-  add_foreign_key 'likes', 'posts'
-  add_foreign_key 'likes', 'users', column: 'author_id'
-  add_foreign_key 'likes', 'users', column: 'author_id'
-  add_foreign_key 'posts', 'users', column: 'author_id'
-  add_foreign_key 'posts', 'users', column: 'author_id'
 end
