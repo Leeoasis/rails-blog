8353887e2c3df44f819fe07cdc17aaf7c3cd6bae
Leslie <Kooliesk@gmail.com>
2023-06-22 02:04:14 +0200

    Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
 
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new create]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
   end
