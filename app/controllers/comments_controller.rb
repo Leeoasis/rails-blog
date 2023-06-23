cf5b277ff1471b46d9bd66357b6c13ceefe7d3a3
M.Nisar Ali <dr.nisaralig@gmail.com>
2023-06-23 17:20:40 +0500

    Add the destroy method in comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   def new
     @post = @current_user.posts.find(params[:post_id])
   end
-
   def create
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
@@ -14,9 +14,12 @@ class CommentsController < ApplicationController
       render :new
     end
   end
-
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to request.referrer
+  end
   private
-
   def comment_params
     params.require(:comment).permit(:text)
   end
