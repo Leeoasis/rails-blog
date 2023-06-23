ea2d9eaf82326f60b3afa6373aaacd831d534d7d
Leslie <Kooliesk@gmail.com>
2023-06-23 15:15:46 +0200

    fix rubocop errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
-  # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'comments', force: :cascade do |t|
@@ -23,6 +22,10 @@ ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
     t.index ['author_id'], name: 'index_comments_on_author_id'
     t.index ['post_id'], name: 'index_comments_on_post_id'
   end
+end
+
+ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
+  enable_extension 'plpgsql'
 
   create_table 'likes', force: :cascade do |t|
     t.datetime 'created_at', null: false
@@ -32,6 +35,10 @@ ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
     t.index ['author_id'], name: 'index_likes_on_author_id'
     t.index ['post_id'], name: 'index_likes_on_post_id'
   end
+end
+
+ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
+  enable_extension 'plpgsql'
 
   create_table 'posts', force: :cascade do |t|
     t.string 'title'
@@ -44,6 +51,10 @@ ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
     t.integer 'comments_counter', default: 0
     t.index ['author_id'], name: 'index_posts_on_author_id'
   end
+end
+
+ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
+  enable_extension 'plpgsql'
 
   create_table 'users', force: :cascade do |t|
     t.string 'name'
@@ -64,7 +75,9 @@ ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
     t.string 'role'
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+end
 
+ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
   add_foreign_key 'comments', 'posts'
   add_foreign_key 'comments', 'users', column: 'author_id'
   add_foreign_key 'likes', 'posts'
