d258a9b49a2c6e0af153698b27d56ac959bfcbbe
Leslie <Kooliesk@gmail.com>
2023-06-23 11:39:02 +0200

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new create]
-      resources :likes, only: [:create]
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create, :destroy]
+    end
+  end
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'users#index', as: :unauthenticated_root
     end
   end
 end
