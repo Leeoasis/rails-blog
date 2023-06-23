f8334ade80417e09624dfb1da248f6344b1a0268
Leslie <Kooliesk@gmail.com>
2023-06-23 16:31:39 +0200

    update application.rb file

--- config/application.rb
+++ config/application.rb
@@ -1,24 +1,24 @@
 require_relative 'boot'
 require 'rails/all'
 Bundler.require(*Rails.groups)
+
 module RailsBlog
   class Application < Rails::Application
     config.load_defaults 7.0
-    # Add the configuration settings below this line
-    config.middleware.use ActionDispatch::ContentNegotiation
+
+    # Add other middleware configurations here
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
     config.middleware.use ActionDispatch::Flash
+
     # Set JSON as the default API format
     config.api_only = true
     config.debug_exception_response_format = :api
-    # End of configuration settings
+
     # Configuration for the application, engines, and railties goes here.
-    #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)" 23vb
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
