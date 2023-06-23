d3e426bb4da1f9a6f28fdee4a106260612c9c779
Leslie <Kooliesk@gmail.com>
2023-06-23 15:01:39 +0200

    Fix rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -6,21 +6,21 @@ Rails.application.routes.draw do
       get 'posts/index'
       resources :users, only: [] do
         resources :posts, only: [:index] do
-          resources :comments, only: [:index, :create]
+          resources :comments, only: %i[index create]
         end
       end
     end
   end
-  
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:create, :destroy]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[new create destroy]
+      resources :likes, only: %i[create destroy]
     end
   end
   devise_scope :user do
--- db/schema.rb
+++ db/schema.rb
@@ -10,66 +10,79 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_151112) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
+  enable_extension 'plpgsql'
 
-  create_table "comments", force: :cascade do |t|
-    t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
+  create_table 'comments', force: :cascade do |t|
+    t.string 'text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'author_id', null: false
+    t.bigint 'post_id', null: false
+    t.index ['author_id'], name: 'index_comments_on_author_id'
+    t.index ['post_id'], name: 'index_comments_on_post_id'
   end
+end
 
-  create_table "likes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
-    t.index ["author_id"], name: "index_likes_on_author_id"
-    t.index ["post_id"], name: "index_likes_on_post_id"
+ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
+  enable_extension 'plpgsql'
+
+  create_table 'likes', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'author_id', null: false
+    t.bigint 'post_id', null: false
+    t.index ['author_id'], name: 'index_likes_on_author_id'
+    t.index ['post_id'], name: 'index_likes_on_post_id'
   end
+end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "text"
-    t.integer "comment_counter"
-    t.integer "likes_counter", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.integer "comments_counter", default: 0
-    t.index ["author_id"], name: "index_posts_on_author_id"
+ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
+  enable_extension 'plpgsql'
+
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.string 'text'
+    t.integer 'comment_counter'
+    t.integer 'likes_counter', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'author_id', null: false
+    t.integer 'comments_counter', default: 0
+    t.index ['author_id'], name: 'index_posts_on_author_id'
   end
+end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.string "bio"
-    t.integer "posts_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "role"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
+  enable_extension 'plpgsql'
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'photo'
+    t.string 'bio'
+    t.integer 'posts_counter'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.string 'role'
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
+ActiveRecord::Schema[7.0].define(version: 20_230_622_151_112) do
+  add_foreign_key 'comments', 'posts'
+  add_foreign_key 'comments', 'users', column: 'author_id'
+  add_foreign_key 'likes', 'posts'
+  add_foreign_key 'likes', 'users', column: 'author_id'
+  add_foreign_key 'likes', 'users', column: 'author_id'
+  add_foreign_key 'posts', 'users', column: 'author_id'
+  add_foreign_key 'posts', 'users', column: 'author_id'
 end
