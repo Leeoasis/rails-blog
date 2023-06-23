24d9b579b398262d5d7900ffc74bd0b271c1988e
Leslie <Kooliesk@gmail.com>
2023-06-23 11:39:18 +0200

    Add role migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,77 +10,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-class CreateComments < ActiveRecord::Migration[7.0]
-  enable_extension 'plpgsql'
-  def change
-    create_table 'comments', force: :cascade do |t|
-      t.string 'text'
-      t.datetime 'created_at', null: false
-      t.datetime 'updated_at', null: false
-      t.bigint 'author_id', null: false
-      t.bigint 'post_id', null: false
-      t.index ['author_id'], name: 'index_comments_on_author_id'
-      t.index ['post_id'], name: 'index_comments_on_post_id'
-    end
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_151112) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-    add_foreign_key 'comments', 'posts'
-    add_foreign_key 'comments', 'users', column: 'author_id'
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
-end
 
-class CreateLikes < ActiveRecord::Migration[7.0]
-  def change
-    create_table 'likes', force: :cascade do |t|
-      t.datetime 'created_at', null: false
-      t.datetime 'updated_at', null: false
-      t.bigint 'author_id', null: false
-      t.bigint 'post_id', null: false
-      t.index ['author_id'], name: 'index_likes_on_author_id'
-      t.index ['post_id'], name: 'index_likes_on_post_id'
-    end
-
-    add_foreign_key 'likes', 'posts'
-    add_foreign_key 'likes', 'users', column: 'author_id'
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
-end
 
-class CreatePosts < ActiveRecord::Migration[7.0]
-  def change
-    create_table 'posts', force: :cascade do |t|
-      t.string 'title'
-      t.string 'text'
-      t.integer 'comment_counter'
-      t.integer 'likes_counter', default: 0
-      t.datetime 'created_at', null: false
-      t.datetime 'updated_at', null: false
-      t.bigint 'author_id', null: false
-      t.integer 'comments_counter', default: 0
-      t.index ['author_id'], name: 'index_posts_on_author_id'
-    end
-
-    add_foreign_key 'posts', 'users', column: 'author_id'
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.integer "comment_counter"
+    t.integer "likes_counter", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.integer "comments_counter", default: 0
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
-end
 
-class CreateUsers < ActiveRecord::Migration[7.0]
-  def change
-    create_table 'users', force: :cascade do |t|
-      t.string 'name'
-      t.string 'photo'
-      t.string 'bio'
-      t.integer 'posts_counter'
-      t.datetime 'created_at', null: false
-      t.datetime 'updated_at', null: false
-      t.string 'email', default: '', null: false
-      t.string 'encrypted_password', default: '', null: false
-      t.string 'reset_password_token'
-      t.datetime 'reset_password_sent_at'
-      t.datetime 'remember_created_at'
-      t.string 'confirmation_token'
-      t.datetime 'confirmed_at'
-      t.datetime 'confirmation_sent_at'
-      t.string 'unconfirmed_email'
-      t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-    end
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.string "bio"
+    t.integer "posts_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "role"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
